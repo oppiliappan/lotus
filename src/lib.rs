d0fe92af8696ff4d8c1fe4196209d2d693933c34
NerdyPepper <akshayoppiliappan@gmail.com>
2019-04-23 15:03:24 +0530

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -16,12 +16,22 @@ pub struct Lotus<'a> {
     precision: u8,
     thousand_str: &'a str,
     decimal_str: &'a str,
-    format: &'a str,
+    format_positive: &'a str,
     format_negative: &'a str,
     format_zero: &'a str
 }
 
 impl<'a> Lotus<'a> {
+    /// Creates a Lotus instance with the give symbol and precision
+    /// and precision
+    ///
+    /// # Example:
+    /// ```
+    /// use crate::Lotus::*;
+    /// let dollar = Lotus::new("$", 2);
+    /// let f = dollar.format(3500);
+    /// assert_eq!("$ 3,500.00", f);
+    /// ```
     pub fn new(symbol: &str, precision: u8) -> Lotus {
         Lotus {
             symbol,
@@ -30,6 +40,24 @@ impl<'a> Lotus<'a> {
         }
     }
 
+    /// Formats a (generic) number according to the object 
+    /// configuration
+    ///
+    /// # Example:
+    /// ```
+    /// use crate::Lotus::*;
+    /// let rupee = LotusBuilder::default()
+    ///     .symbol("Rs.")
+    ///     .precision(4)
+    ///     .format_positive("%s %v")
+    ///     .format_negative("%s (%v)")
+    ///     .format_zero("%s 0.00")
+    ///     .decimal_str(".")
+    ///     .thousand_str(" ")
+    ///     .build()
+    ///     .unwrap();
+    /// assert_eq!("Rs. 2 000 000.0000", rupee.format(2_000_000));
+    /// ```
     pub fn format<T: Into<f64> + Display>(&self, in_number: T) -> String {
         let number: f64 = in_number.into();
         if number == 0. {
@@ -58,33 +86,11 @@ impl<'a> Lotus<'a> {
             if number.is_sign_negative() {
                 currencied.push_str(self.format_negative);
             } else {
-                currencied.push_str(self.format);
+                currencied.push_str(self.format_positive);
             }
             currencied = currencied.replace("%v", formatted_float.as_str());
             currencied = currencied.replace("%s", &self.symbol[..]);
             return currencied;
-        }
-    }
-}
-
-#[macro_export]
-macro_rules! lotus {
-    ($x:expr, $y:expr) => {
-        {
-            let quick = LotusBuilder::default()
-                .symbol($y)
-                .build()
-                .unwrap();
-            quick.format($x)
-        }
-    };
-    ($x: expr) => {
-        {
-            let quick = LotusBuilder::default()
-                .format("%v")
-                .build()
-                .unwrap();
-            quick.format($x)
         }
     }
 }
@@ -96,7 +102,7 @@ impl<'a> Default for Lotus<'a> {
             precision: 2,
             thousand_str: ",",
             decimal_str: ".",
-            format: "%s %v",
+            format_positive: "%s %v",
             format_negative: "%s (%v)",
             format_zero: "%s --",
         }
