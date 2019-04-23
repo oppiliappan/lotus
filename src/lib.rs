4ceee84035fc8101de7bd685c99a070eabd307e7
NerdyPepper <akshayoppiliappan@gmail.com>
2019-04-23 19:21:23 +0530

    add contained docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+//! # Lotus
+//! `Lotus` is a simple library to provide formatting for currency and numbers.  
+//! It makes pretty printing of monetary values convinient
+//!
+//! # Usage
+//! ```
+//! # #[macro_use]
+//! # use crate::Lotus::*;
+//! // Builder format (recommended)
+//! let rupee = LotusBuilder::default()
+//!     .symbol("Rs.")
+//!     .precision(1)
+//!     .format_positive("%s %v")
+//!     .format_negative("%s (%v)")
+//!     .format_zero("%s 0.00")
+//!     .decimal_str(".")
+//!     .thousand_str(" ")
+//!     .build()
+//!     .unwrap();
+//! assert_eq!("Rs. 2 000 000.0", rupee.format(2_000_000));
+//! assert_eq!("Rs. (2 000.0)", rupee.format(-2000));
+//! assert_eq!("Rs. 0.00", rupee.format(0));
+//!
+//! // Using Lotus::new()
+//! let dollar = Lotus::new("$", 3); // Lotus::new(symbol, precision)
+//! assert_eq!("$ 50,000.035", dollar.format(50_000.035));
+//!
+//! // Using lotus! macro
+//! let f = lotus!(150, "$");     // lotus!(number, symbol)
+//! assert_eq!("$ 150.00", f);
+//! 
+//! let g = lotus!(2_000_000);    // lotus!(number)
+//! assert_eq!("2,000,000.00", g);
+//! ```
+
 use std::default::Default;
 use std::fmt::Display;
 
@@ -7,7 +42,6 @@ extern crate derive_builder;
 #[macro_use]
 mod macros;
 mod tests;
-
 
 #[derive(Debug, Builder)]
 #[builder(default)]
