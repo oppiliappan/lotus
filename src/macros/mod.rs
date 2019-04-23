559f945ea97e80aff8a7c50193f8b3a8847ab032
NerdyPepper <akshayoppiliappan@gmail.com>
2019-04-23 19:21:35 +0530

    add macro docs

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -1,5 +1,23 @@
 mod macros {
+    /// Quality of life formatting macros.
     #[macro_export]
+    ///
+    ///
+    /// The `lotus` macro can
+    /// - format currency (Ex: $ 2,000.0)
+    /// - format numbers (Ex: 2,000.0)
+    /// # Example
+    /// ```
+    /// # #[macro_use]
+    /// # use crate::Lotus::*;
+    ///
+    /// let f = lotus!(150, "$");     // lotus!(number, symbol)
+    /// assert_eq!("$ 150.00", f);
+    /// 
+    /// let g = lotus!(2_000_000);    // lotus!(number)
+    /// assert_eq!("2,000,000.00", g);
+    /// ```
+    ///
     macro_rules! lotus {
         ($x:expr, $y:expr) => {
             {
